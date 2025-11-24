0a63899f8d693863016e08b6b8d2304bf8dd9812
Check-HazratAli <checkhazratali@gmail.com>
2025-11-24 16:01:28 +0600

    introduction_slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_widgets/onboarding_widgets/onboarding_animation/onboarding_animation.dart';
+import 'package:flutter_onboarding_widgets/onboarding_widgets/gone_board/gone_board.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'OnBoarding Animation',
-      home: OnBoardingAnimationExample(),
-    );
-  }
-}
+void main() => runApp(MaterialApp(home: MyApp()));
 
