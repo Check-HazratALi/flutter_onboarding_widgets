695d1527401d2ff90f2853f327cfba66870d46ac
Check-HazratAli <checkhazratali@gmail.com>
2025-11-24 14:56:43 +0600

    intro_sliderintro_slider_screens

---
+++ lib/onboarding_widgets/intro_slider/main.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'onboarding_widgets/intro_slider/intro_screen_default.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home:  IntroScreenDefault(),// IntroScreenCustomConfig(),// IntroScreenCustomLayout(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+
+
